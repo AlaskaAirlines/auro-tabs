b6638f300ea52dc235338ffa65e2fd2e190d9fcf
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2023-06-28 17:15:09 +0700

    chore: ui state support #2
    Changes to be committed:
    	modified:   src/auro-tab.js
    	modified:   src/auro-tabgroup.js
    	modified:   src/style.scss
    	modified:   src/tab-style.scss

--- src/auro-tab.js
+++ src/auro-tab.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 // Copyright (c) 2023 Alaska Airlines. All right reserved. Licensed under the Apache-2.0 license
 // See LICENSE in the project root for license information.
 
@@ -16,12 +17,76 @@ import styleCss from "./tab-style-css.js";
 
 // build the component class
 export class AuroTab extends AuroHyperlink {
+  constructor() {
+    super();
+
+    this.disabled = false;
+  }
+
+  // This function is to define props used within the scope of this component
+  // Be sure to review  https://lit.dev/docs/components/properties/
+  // to understand how to use reflected attributes with your property settings.
+  static get properties() {
+    return {
+      ...super.properties,
+      selected: {
+        type: Boolean,
+        reflect: true
+      },
+      disabled: {
+        type: Boolean,
+        reflect: true
+      },
+    };
+  }
+
   static get styles() {
     return [styleCss];
   }
 
   firstUpdated() {
-    this.setAttribute('role', 'tab');
+    if (!this.href) {
+      this.setAttribute('role', 'tab');
+    }
+
+    // Set a well-defined initial state.
+    this.setAttribute('aria-selected', 'false');
+    this.setAttribute('tabindex', -1);
+    this.upgradeProperty('selected');
+  }
+
+  upgradeProperty(prop) {
+    if (Object.hasOwn(this, prop)) {
+      const value = this[prop];
+      delete this[prop];
+      this[prop] = value;
+    }
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    super.attributeChangedCallback(name, oldVal, newVal);
+    const value = this.hasAttribute('selected');
+    this.setAttribute('aria-selected', value);
+
+    this.setAttribute('tabindex', value ? 0 : -1);
+  }
+
+  set selected(value) {
+    if (value) {
+      this.setAttribute('selected', '');
+      const event = new Event('tab-selected', {
+        bubbles: true,
+        composed: true,
+        detail: this
+      });
+      this.dispatchEvent(event);
+    } else {
+      this.removeAttribute('selected');
+    }
+  }
+
+  get selected() {
+    return this.hasAttribute('selected');
   }
 }
 
--- src/auro-tabgroup.js
+++ src/auro-tabgroup.js
@@ -5,7 +5,7 @@
 
 // If using litElement base class
 import { LitElement, html } from "lit";
-// import { styleMap } from 'lit/directives/style-map.js';
+import { styleMap } from 'lit/directives/style-map.js';
 
 // Import touch detection lib
 import styleCss from "./style-css.js";
@@ -23,9 +23,13 @@ import styleCss from "./style-css.js";
 
 // build the component class
 export class AuroTabgroup extends LitElement {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+
+    this.sliderStyles = {};
+
+    this.addEventListener('tab-selected', () => this.setSliderStyles());
+  }
 
   // This function is to define props used within the scope of this component
   // Be sure to review  https://lit.dev/docs/components/properties/
@@ -40,19 +44,187 @@ export class AuroTabgroup extends LitElement {
     return [styleCss];
   }
 
-  firstUpdated() {
+  async firstUpdated() {
     if (!this.hasAttribute('role')) {
       this.setAttribute('role', 'tablist');
     }
+
+    this.addEventListener('keydown', this.onKeyDown);
+    this.addEventListener('click', this.onClick);
+
+    await Promise.all([
+      customElements.whenDefined('auro-tab'),
+      customElements.whenDefined('auro-tabpanel'),
+    ]);
+
+    this.setSliderStyles();
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('keydown', this.onKeyDown);
+    this.removeEventListener('click', this.onClick);
+  }
+
+  allPanels() {
+    return Array.from(this.querySelectorAll('auro-tabpanel'));
+  }
+
+  allTabs() {
+    return Array.from(this.querySelectorAll('auro-tab'));
+  }
+
+  prevTab() {
+    const tabs = this.allTabs();
+    // Use `findIndex()` to find the index of the currently
+    // selected element and subtracts one to get the index of the previous
+    // element.
+    const newIdx =
+      tabs.findIndex((tab) => tab.selected) - 1;
+    // Add `tabs.length` to make sure the index is a positive number
+    // and get the modulus to wrap around if necessary.
+    return tabs[(newIdx + tabs.length) % tabs.length];
+  }
+
+  nextTab() {
+    const tabs = this.allTabs();
+    const newIdx = tabs.findIndex((tab) => tab.selected) + 1;
+    return tabs[newIdx % tabs.length];
+  }
+
+  firstTab() {
+    const tabs = this.allTabs();
+    return tabs[0];
+  }
+
+  lastTab() {
+    const tabs = this.allTabs();
+    return tabs[tabs.length - 1];
+  }
+
+  reset() {
+    const tabs = this.allTabs();
+
+    tabs.forEach((tab) => {
+      tab.selected = false;
+    });
+  }
+
+  selectTab(newTab) {
+    // Deselect all tabs and hide all panels.
+    this.reset();
+    newTab.selected = true;
+
+    const tabs = this.allTabs();
+    this.focusedTabIdx = tabs.findIndex((tab) => tab === newTab);
+  }
+
+  onKeyDown(event) {
+    const KEYCODE = {
+      DOWN: 'ArrowDown',
+      LEFT: 'ArrowLeft',
+      RIGHT: 'ArrowRight',
+      UP: 'ArrowUp',
+      HOME: 'Home',
+      END: 'End',
+      ENTER: 'Enter',
+      SPACE: ' ',
+    };
+
+    // Don’t handle modifier shortcuts typically used by assistive technology.
+    if (event.altKey) {
+      return;
+    }
+
+    // The switch-case will determine which tab should be marked as focused
+    // depending on the key that was pressed.
+    const tabs = this.allTabs();
+    let newTab = null;
+    switch (event.key) {
+      case KEYCODE.LEFT:
+      case KEYCODE.UP:
+        if (this.focusedTabIdx === 0) {
+          this.focusedTabIdx = tabs.length - 1;
+        } else {
+          this.focusedTabIdx -= 1;
+        }
+        newTab = tabs[this.focusedTabIdx];
+        break;
+      case KEYCODE.RIGHT:
+      case KEYCODE.DOWN:
+        if (this.focusedTabIdx === tabs.length - 1) {
+          this.focusedTabIdx = 0;
+        } else {
+          this.focusedTabIdx += 1;
+        }
+        newTab = tabs[this.focusedTabIdx];
+        break;
+      case KEYCODE.HOME:
+        newTab = this.firstTab();
+        break;
+      case KEYCODE.END:
+        newTab = this.lastTab();
+        break;
+      case KEYCODE.ENTER:
+      case KEYCODE.SPACE:
+        newTab = tabs[this.focusedTabIdx];
+        this.selectTab(newTab);
+        break;
+      // Any other key press is ignored and passed back to the browser.
+      default:
+        return;
+    }
+
+    // The browser might have some native functionality bound to the arrow
+    // keys, home or end. The element calls `preventDefault()` to prevent the
+    // browser from taking any actions.
+    event.preventDefault();
+    // Focus to the new tab, that has been determined in the switch-case.
+    newTab.focus();
+  }
+
+  /**
+   * @param {Event} event HTML click Event.
+   * `onClick()` handles clicks inside the tab panel.
+   */
+  onClick(event) {
+    const roleIsNotTab = event.target.getAttribute('role') !== 'tab';
+    const closestTab = event.target.closest(['[role=tab]']);
+
+    // If the click was not targeted on a tab element itself,
+    // it was a click inside the a panel or on empty space. Nothing to do.
+    // actually this is going to be known issue for custom auro-tab component name in the future
+    if (roleIsNotTab && !closestTab && event.target.localName !== 'auro-tab') {
+      return;
+    }
+
+    if (closestTab) {
+      this.selectTab(closestTab);
+    } else {
+      this.selectTab(event.target);
+    }
+  }
+
+  setSliderStyles () {
+    this.sliderStyles.width = 0;
+    const tabs = this.allTabs();
+
+    const activeTab = tabs.find((tab) => tab.selected);
+    this.sliderStyles = {
+      width: activeTab ? `${activeTab.clientWidth}px` : 0,
+      left: activeTab ? `${activeTab.offsetLeft}px` : 0,
+    };
+    this.requestUpdate();
   }
 
   // function that renders the HTML and CSS into the scope of the component
   render() {
+    const sliderStyles = styleMap(this.sliderStyles);
+
     return html`
     <div class="tabgroupContainer">
       <div class="tabgroup">
         <slot name="tab"></slot>
-        <div class="slider"></div>
+        <div class="slider" style=${sliderStyles}></div>
       </div>
     </div>
     <slot name="panel"></slot>
