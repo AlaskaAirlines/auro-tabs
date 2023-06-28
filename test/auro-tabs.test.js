83f8d2de4e21db57b0f7869722b0b73bb9eb9862
fajar-apri-alaska <Fajar.Apriansyah@alaskaair.com>
2023-06-28 12:05:41 +0700

    test: update test script for unused test
    Changes to be committed:
    	modified:   test/auro-tabs.test.js

--- test/auro-tabs.test.js
+++ test/auro-tabs.test.js
@@ -1,26 +1,17 @@
 import { fixture, html, expect } from '@open-wc/testing';
-import '../src/auro-tabs';
+import '../src/auro-tabgroup';
 
-describe('auro-tabs', () => {
-  it('sets the CSS class on auro-tabs > div element', async () => {
+describe('auro-tabgroup', () => {
+  it('auro-tabgroup is accessible', async () => {
     const el = await fixture(html`
-      <auro-tabs cssclass="testClass"></auro-tabs>
-    `);
-
-    const div = el.shadowRoot.querySelector('div');
-    expect(div.className).to.equal('testClass');
-  });
-
-  it('auro-tabs is accessible', async () => {
-    const el = await fixture(html`
-      <auro-tabs cssclass="testClass"></auro-tabs>
+      <auro-tabgroup cssclass="testClass"></auro-tabgroup>
     `);
 
     await expect(el).to.be.accessible();
   });
 
-  it('auro-tabs custom element is defined', async () => {
-    const el = await !!customElements.get("auro-tabs");
+  it('auro-tabgroup custom element is defined', async () => {
+    const el = await !!customElements.get("auro-tabgroup");
 
     await expect(el).to.be.true;
   });
