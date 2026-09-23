3433266687842432c2dda29b7c54938fb569cd06
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-09-23 23:10:53 +0000

    Remove preview for PR 98 🛬

--- pr-preview/pr-98/api.js
+++
@@ -1,8 +0,0 @@
-import "../src/registered";
-import { addListener } from "./utils/onTabSelected";
-
-// Register the listener
-addListener();
-
-export * from "./utils/onTabSelected";
-export * from "./utils/selectTabExample";
